88e7f004359e9fb92afb43f86f5bd879c62adabc
cstfre <84088109+cstfre@users.noreply.github.com>
2023-08-15 20:12:12 +0300

    C-Functions

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * fibonacci_103
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {

4cff739738650c192e27bd09dfe2b89fe05b225b
cstfre <84088109+cstfre@users.noreply.github.com>
2023-08-15 20:04:00 +0300

    C-Functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * natural
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {

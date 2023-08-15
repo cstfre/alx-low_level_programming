19ac65ada9b5ac8443ab4209a158a5fe85b9f696
cstfre <84088109+cstfre@users.noreply.github.com>
2023-08-15 20:17:25 +0300

    C-Functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * P Last dig
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
  */
 int print_last_digit(int n)
 {

fc4eee18079adcbf766d92befc9e8992ee055af3
cstfre <84088109+cstfre@users.noreply.github.com>
2023-08-15 20:14:37 +0300

    C-Functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * i slower
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
